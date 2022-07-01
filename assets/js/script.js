7624e99edb3e02b3fc457f627c38d0b56b009c8d
Tyler <tyler.rodgers74@gmail.com>
2022-07-01 05:05:48 -0400

    added ability to save/load events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+$("#currentDay").html(moment().format("dddd, MMMM Do"));
+
+var eventArr = {};
+
 $(".time-block").on("click", "p", function () {
   var text = $(this).text().trim();
 
@@ -18,7 +22,23 @@ $(".saveBtn").on("click", function (event) {
       .addClass("col-9 border border-light p-3 text-input mr-0 p-0 description")
       .text(text);
     $(this).prev().replaceWith(taskP);
+
+    var text = $(this).prev().text();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
   }
 });
 
-$("#currentDay").html(moment().format("dddd, MMMM Do"));
+function loadEvents() {
+  $("#1 .description").text(localStorage.getItem("1"));
+  $("#2 .description").text(localStorage.getItem("2"));
+  $("#3 .description").text(localStorage.getItem("3"));
+  $("#4 .description").text(localStorage.getItem("4"));
+  $("#5 .description").text(localStorage.getItem("5"));
+  $("#6 .description").text(localStorage.getItem("6"));
+  $("#7 .description").text(localStorage.getItem("7"));
+  $("#8 .description").text(localStorage.getItem("8"));
+  $("#9 .description").text(localStorage.getItem("9"));
+}
+
+loadEvents();
