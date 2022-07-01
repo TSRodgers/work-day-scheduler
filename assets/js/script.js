7721b4cd733db1ea958bc3cbc53bf2362c0202d1
Tyler <tyler.rodgers74@gmail.com>
2022-07-01 03:48:18 -0400

    fixed save button to only save this row it's on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,24 @@
-$(".text-input").on("click", function () {
-  if ($(this).html() === "") {
-    let textInput = $("<textarea>").addClass("text-form");
-    $(this).append(textInput);
-    textInput.trigger("focus");
-    return textInput;
-  }
-});
-
-$(".text-input").on("click", "p", function () {
+$(".time-block").on("click", "p", function () {
   var text = $(this).text().trim();
 
-  let textInput = $("<textarea>").addClass("text-form").val(text);
+  let textInput = $("<textarea>")
+    .addClass("col-9 w-100 h-100 text-input")
+    .val(text);
   $(this).replaceWith(textInput);
 
   textInput.trigger("focus");
-  return textInput;
 });
 
-$(".time-block").on("click", ".saveBtn", function () {
-  var text = $(".text-form").val();
-
-  var taskP = $("<p>").addClass().text(text);
-
-  $(".text-form").replaceWith(taskP);
+$(".saveBtn").on("click", function (event) {
+  if ($(this).prev().is("p")) {
+    return false;
+  } else {
+    var text = $(this).prev().val();
+    var taskP = $("<p>")
+      .addClass("col-9 border border-light p-3 text-input mr-0 p-0 description")
+      .text(text);
+    $(this).prev().replaceWith(taskP);
+  }
 });
+
+$("#currentDay").html(moment().format("dddd, MMMM Do"));
